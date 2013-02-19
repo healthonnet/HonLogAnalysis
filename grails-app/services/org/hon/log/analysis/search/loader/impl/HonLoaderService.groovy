6fe20012a020ab7b8cb2132d315d20e5b518a9d4
Joao Palotti <joaopalotti@gmail.com>
2013-02-19 14:57:29 +0100

    Revert "KHRES-316 #comment Changed the size of the referer column to 2048 chars. The same limit was logically added into the code."
    This reverts commit 11e2dae1fa32f3ecd6cd210222ae4bbc80fbc9fd.

--- grails-app/services/org/hon/log/analysis/search/loader/impl/HonLoaderService.groovy
+++ grails-app/services/org/hon/log/analysis/search/loader/impl/HonLoaderService.groovy
@@ -103,8 +103,6 @@ class HonLoaderService extends SearchLogLineLoaderAbst{
 		String engine = findUrlDecodedIfRegexMatches(patternEngine, rawQuery);
 		String query = findUrlDecodedIfRegexMatches(patternQuery, rawQuery, engine);
 		Map parsedQuery = parseQuery(myLine2Map.query, engine)
-		// A static (and large) limit for the referer
-		String referer = myLine2Map.referer.size() > 2048 ? myLine2Map.referer[0..2047] : myLine2Map.referer 
 			
 		def searchLogLine = new SearchLogLine(
 				source:source,
@@ -114,7 +112,7 @@ class HonLoaderService extends SearchLogLineLoaderAbst{
 				termList:parsedQuery.terms,
 				origQuery:query,
 				engine: engine,
-				referer:referer,
+				referer:myLine2Map.referer,
 				)
 		
 		associateSearchLogLineWithRemoteIp(searchLogLine, myLine2Map.remoteIp);
